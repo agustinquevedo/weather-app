d3bfb25faa6e27009463329c62260c1dc996fde7
Agustín Quevedo <agustinquevedo@outlook.com>
2020-09-01 18:56:34 +0200

    PropTypes

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeathererTemperature from './WeatherTemperature';
+import { SUNNY } from '../const/weather';
 
 const WeatherData = () => (
     <div>
         <WeathererTemperature
             temperature={20}
-            weatherState={''}
+            weatherState={SUNNY}
         />
         <WeatherExtraInfo humidity={70} wind={"10 m/s"}/>
     </div>
--- src/components/WeatherTemperature.js
+++ src/components/WeatherTemperature.js
@@ -1,27 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 // import the library
-import { library } from '@fortawesome/fontawesome-svg-core';
-
+// import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSun, faCloudSun } from '@fortawesome/free-solid-svg-icons'
-
-library.add(
-    faSun,
-    faCloudSun
-);
+import { faSun, faCloudSun, faCloud, faCloudRain, faSnowflake, faWind} from '@fortawesome/free-solid-svg-icons'
+import {
+    CLOUD,
+    CLOUDY,
+    SUNNY,
+    RAIN,
+    SNOW,
+    WINDY,
+} from '../const/weather';
 
 const icons = {
-    sunny: "faSun",
-    fog: "faCloudSun"
+    [CLOUD]: faCloud,
+    [CLOUDY]: faCloudSun,
+    [SUNNY]: faSun,
+    [RAIN]: faCloudRain,
+    [SNOW]: faSnowflake,
+    [WINDY]: faWind
 }
 
 const getFontAwesomeIcon = weatherState => {
     const icon = icons[weatherState]
-    if (icon) 
+    if (icon)
         return <FontAwesomeIcon icon={icon} size="lg"/>
-    else
+    else 
         return <FontAwesomeIcon icon={faSun} size="lg"/>
-}
+};
 
 const WeatherTemperature = ({temperature, weatherState}) => {
     return (
@@ -31,5 +38,11 @@ const WeatherTemperature = ({temperature, weatherState}) => {
         </div>
     )
 };
+
+// PropTypes validation
+WeatherTemperature.propTypes = {
+    temperature: PropTypes.number.isRequired,
+    weatherState: PropTypes.string.isRequired
+}
 
 export default WeatherTemperature;
